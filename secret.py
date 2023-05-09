56e531ad99f7d7feafa0c807fb05b7a8970d3016
Chen Ching Yen <92667133+ChenChingYen@users.noreply.github.com>
2023-05-09 21:38:45 +0800

    hide secret

--- secret.py
+++ secret.py
@@ -1,7 +1,3 @@
 secret = {
-    "API_KEY_FIREBASE": "AIzaSyAfTgXDqIRuroN2727soKdVaCKAE7WkOHs",
-    "DATABASE_URL_FIREBASE": "https://streamlit-c092c-default-rtdb.asia-southeast1.firebasedatabase.app",
-    "APP_ID_FIREBASE": "1:761273431495:web:e93508bc5300a29c6955e2",
-    "API_KEY_APIFY": 'apify_api_q1geHnugEuR5tySZZE0jBffGq4y8eN2yGIeH',
-    "ACTOR_IP_APIFY": "ryoshi_007~my-actor"
+
 }
