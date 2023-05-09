c8cce2a785b50779a992c329da483826f842eba1
Wong Yan Jian <yanjian1510@gmail.com>
2023-05-09 22:55:42 +0800

    update secrets

--- firebaseConfig.py
+++ firebaseConfig.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 config = {
-  "apiKey": st.secret['API_KEY_FIREBASE'],
+  "apiKey": st.secrets['API_KEY_FIREBASE'],
   "authDomain": "streamlit-c092c.firebaseapp.com",
-  "databaseURL": st.secret['DATABASE_URL_FIREBASE'],
+  "databaseURL": st.secrets['DATABASE_URL_FIREBASE'],
   "projectId": "streamlit-c092c",
   "storageBucket": "streamlit-c092c.appspot.com",
   "messagingSenderId": "761273431495",
-  "appId": st.secret['APP_ID_FIREBASE'],
+  "appId": st.secrets['APP_ID_FIREBASE'],
   "measurementId": "G-HRX77LVPRX"
 }
